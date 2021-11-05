592fe074c9dbd3382f15f67ebb925ff424b7b9c2
hasnain sikandar <s20755753@gmail.com>
2021-11-05 06:10:22 +0500

    add some models and initial works

---
+++ mess_manager/backend/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+# normal user
+class User(AbstractUser):
+    """add some more field to user model"""
+
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    city = models.CharField(max_length=20, blank=True, null=True)
+    hostel = models.CharField(max_length=20, blank=True, null=True)
+    room = models.CharField(max_length=20, blank=True, null=True)
+    image = models.ImageField(upload_to='profile_image', blank=True, null=True)
+
+
+class Message(models.Model):
+    """
+    Message model
+    """
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.message
+
+
+
+class Attendance(models.Model):
+    """
+    Attendance model
+    """
+    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.student.name + ' ' + self.date.strftime('%d/%m/%Y') + ' ' + self.time.strftime('%H:%M') + ' ' + self.status
+
+
+# mess menu
+class Menu(models.Model):
+    """
+    Menu model
+    """
+    name = models.CharField(max_length=100)
+    time = models.TimeField()
+
+
+
+    def __str__(self):
+        return self.name
+
+
+
+# admin
+class Admin(models.Model):
+    """
+    Admin model
+    """
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    id = models.AutoField(primary_key=True)
+
+
+    def __str__(self):
+        return self.name
+
+
